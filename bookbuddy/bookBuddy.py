6032f4577e1ac628586c0d067d8790362775a7c0
lucifer1708 <20bce091@nith.ac.in>
2023-04-08 15:54:42 +0530

    bookbuddy view ok

--- bookbuddy/bookBuddy.py
+++ bookbuddy/bookBuddy.py
@@ -1,15 +1,11 @@
 from PyPDF2 import PdfReader
-from gptKaam import GPT_3, load_dotenv
-import os
 from gtts import gTTS
 from io import BytesIO
-from sumy.summarizers.luhn import LuhnSummarizer
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
+# from sumy.summarizers.luhn import LuhnSummarizer
+# from sumy.parsers.plaintext import PlaintextParser
+# from sumy.nlp.tokenizers import Tokenizer
 
 #  can modify to get detail by page
-load_dotenv()
-gpt_3 = GPT_3(os.getenv('OPENAI_API_KEY'))
 
 total_content = ""
 
@@ -26,13 +22,13 @@ def getText(file_path, total_content):
     return val, total_content
 
 
-val, text = getText("know-india-booklet.pdf", "")
-parser = PlaintextParser(text, Tokenizer('english'))
-summarizer_1 = LuhnSummarizer()
-summary_1 = summarizer_1(parser.document, 10)
-print(summary_1)
-for sentence in summary_1:
-    print(sentence)
+# val, text = getText("", "")
+# parser = PlaintextParser(text, Tokenizer('english'))
+# summarizer_1 = LuhnSummarizer()
+# summary_1 = summarizer_1(parser.document, 10)
+# print(summary_1)
+# for sentence in summary_1:
+#     print(sentence)
 
 
 def sumToAudio(summary):
@@ -40,3 +36,9 @@ def sumToAudio(summary):
     af = BytesIO()
     audio.write_to_fp(af)
     return af
+
+
+# def handle_uploaded_file(f):
+#     with open('/static/upload/'+f.name, 'wb+') as destination:
+#         for chunk in f.chunks():
+#             destination.write(chunk)
--- eduGPT/urls.py
+++ eduGPT/urls.py
@@ -14,12 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
+from bookbuddy.views import bookbuddy
 
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
+    path('bookbuddy/', bookbuddy),
     path('accounts/', include('allauth.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT}),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
