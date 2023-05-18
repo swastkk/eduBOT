bc0badf0e4f216eaa83515c8931d34e72afcb454
Swastik Sharma <swastkk@gmail.com>
2023-05-19 04:43:39 +0530

    Unwanted files removed
    Signed-off-by: Swastik Sharma <swastkk@gmail.com>

--- learnit/forms.py
+++ learnit/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class LearnForm(forms.Form):
-    text = forms.CharField(label="What do you want to learn?", max_length=100)
+    text = forms.CharField(label="What do you want to learn?", max_length=100, widget=forms.TextInput(attrs={'class': 'input-text'}))
     MY_CHOICES = (
         ('1', '1 question'),
         ('2', '2 question'),
@@ -10,4 +10,4 @@ class LearnForm(forms.Form):
         ('4', '4 question'),
         ('5', '5 question'),
     )
-    no_of_question_for_quiz = forms.ChoiceField(choices=MY_CHOICES)
+    no_of_question_for_quiz = forms.ChoiceField(choices=MY_CHOICES, widget=forms.Select(attrs={'class': 'input-quiz'}))
