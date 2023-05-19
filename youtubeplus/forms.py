373ff354a7149846aaf7e62cd4a32f41f92ddedd
Swastik Sharma <swastkk@gmail.com>
2023-05-19 18:28:18 +0000

    YT+ UI improved
    Signed-off-by: Swastik Sharma <swastkk@gmail.com>

--- youtubeplus/forms.py
+++ youtubeplus/forms.py
@@ -2,10 +2,10 @@ from django import forms
 
 
 class YtForm(forms.Form):
-    url = forms.CharField(label="Enter your youtube url here", max_length=100)
+    url = forms.CharField(label="Enter youtube URL here", max_length=100)
     lang = forms.CharField(label="Add language", max_length=20)
     MY_CHOICES = (
-        ('5', 'line 5'),
+        ('5',  'lines 5'),
         ('10', 'lines 10'),
         ('15', 'lines 15'),
     )
